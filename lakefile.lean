fbc1daa7c7311804d9d49cbf8b78b7ccdef596e0
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-04 17:16:11 -0300

    Apply suggestions from code review
    Co-authored-by: Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open System Lake DSL
 
 package FFI where
-  moreLinkArgs := #["-lsome_rust_lib", "-L", "./target/debug"]
+  moreLinkArgs := #["-lsome_rust_lib", "-L", "./target/release"]
 
 @[default_target] lean_exe ffi where
   root := `Main
@@ -18,7 +18,7 @@ target importTarget (pkg : Package) : FilePath := do
     compileO ffiC oFile srcFile flags
 
 extern_lib libffi (pkg : Package) := do
-  proc { cmd := "cargo", args := #["build"] }
+  proc { cmd := "cargo", args := #["build", "--release"] }
   let name := nameToStaticLib "ffi"
   let job ← fetch <| pkg.target ``importTarget
   buildStaticLib (pkg.libDir / name) #[job]
